44ba4df9d151db7af2af9c71c8004f52e41bde76
Jon Rutter <contact@jonrutter.io>
2023-02-06 17:33:38 -0500

    Add SearchInput to header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import { SearchInput } from './SearchInput/SearchInput';
 
-export const Header: React.FC = () => (
+type Props = {
+  setQuery?: React.Dispatch<React.SetStateAction<string>>;
+};
+
+/** Renders the site header. */
+export const Header: React.FC<Props> = ({ setQuery }) => (
   <div className="sticky flex top-0 z-40 w-full h-24 bg-zinc-900">
-    <div className="flex justify-between items-center w-full h-full max-w-screen-xl mx-auto px-4">
+    <div className="flex flex-wrap justify-between items-center w-full h-full max-w-screen-xl mx-auto px-4">
       <Link href="/">
         <div className="flex items-center cursor-pointer">
           <div className="hidden md:block">
@@ -25,6 +31,11 @@ export const Header: React.FC = () => (
           </div>
         </div>
       </Link>
+      {setQuery ? (
+        <div className="relative flex items-center">
+          <SearchInput setQuery={setQuery} />
+        </div>
+      ) : null}
     </div>
   </div>
 );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,23 @@
+import React, { useState } from 'react';
 import type { NextPage } from 'next';
 import { Header } from '@/components/Header/Header';
 import { Hero } from '@/components/Hero/Hero';
 import { Grid } from '@/components/Grid/Grid';
 import { Card } from '@/components/Card/Card';
 import { Spinner } from '@/components/Spinner/Spinner';
+import { useFetchMovies } from '@/api/useFetchMovies';
 
 const Home: NextPage = () => {
+  const [query, setQuery] = useState('');
+
+  const { data, fetchNextPage, isLoading, isFetching, error } =
+    useFetchMovies(query);
+
+  console.log(data);
+
   return (
     <main className="relative h-screen overflow-y-scroll">
-      <Header />
+      <Header setQuery={setQuery} />
       <Hero />
       <Grid />
       <Card />
