354a9bc6e2e648ab877cfe4add10d408e07330e7
Jon Rutter <contact@jonrutter.io>
2023-02-07 02:30:45 -0500

    Create Grid component

--- components/Grid/Grid.tsx
+++ components/Grid/Grid.tsx
@@ -1,3 +1,14 @@
 import React from 'react';
 
-export const Grid: React.FC = () => <div>Grid</div>;
+type Props = {
+  title: string;
+  children: React.ReactNode;
+  className?: string;
+};
+
+export const Grid: React.FC<Props> = ({ title, children, className }) => (
+  <div className={className}>
+    <h2 className="text-xl font-bold pb-4">{title}</h2>
+    <div className="grid grid-cols-auto-fill gap-8">{children}</div>
+  </div>
+);
