6b60cd752f3eb6a4ba60d2a6374c05231ad17d9b
Jon Rutter <contact@jonrutter.io>
2023-02-07 02:40:51 -0500

    Extend tailwind theme
    - Extend fontFamily instead of replacing
    
    - Extend grid layout, height, and minHeight

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,18 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
-    fontFamily: {
+    extend: {
+      fontFamily: {
         raleway: ['var(--font-raleway)', 'sans-serif'],
+      },
+      gridTemplateColumns: {
+        'auto-fill': 'repeat(auto-fill, minmax(200px, 1fr))',
+      },
+      height: {
+        128: '40rem',
+      },
+      minHeight: {
+        128: '40rem',
       },
     },
   },
