f0194ed72493a365cfa136f42b4110912315faef
Jon Rutter <contact@jonrutter.io>
2023-02-15 18:46:24 -0500

    Add meta description

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,6 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document';
 const Document = () => (
   <Html lang="en">
     <Head>
+      <meta
+        name="description"
+        content="A Film Database web application. Built with Next.js, and powered by the TMDB API."
+      />
       {/* favicon / manifest */}
       <link rel="manifest" href="/manifest.json" />
       <link rel="apple-touch-icon" href="/logo192.png" />
