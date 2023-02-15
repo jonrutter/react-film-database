9fe06acc97f3a633f6b4ae4ddfc7ff9aefcb9033
Jon Rutter <contact@jonrutter.io>
2023-02-15 02:31:15 -0500

    Update Card component
    - Various styling updates
    
    - Don't pass label to Thumbnail
    
    - Use h3 instead of h2 for heading

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -8,13 +8,13 @@ type Props = {
 };
 
 export const Card: React.FC<Props> = ({ imgUrl, title, subtitle }) => (
-  <div className="h-80">
+  <div className="h-80 rounded-xl shadow-md overflow-hidden">
     <div className="relative h-full">
-      <Thumbnail imgUrl={imgUrl} label={title} />
-      <div className="absolute w-full bottom-0 px-4 py-2 rounded-b-xl bg-zinc-800">
-        <h2 className="text-cyan-200 text-center text-sm truncate">{title}</h2>
+      <Thumbnail imgUrl={imgUrl} />
+      <div className="absolute w-full bottom-0 px-4 py-2 bg-slate-800">
+        <h3 className="text-white text-center text-lg truncate">{title}</h3>
         {subtitle ? (
-          <p className="text-cyan-400 text-center text-sm truncate">
+          <p className="text-cyan-300 text-center text-sm truncate">
             {subtitle}
           </p>
         ) : null}
