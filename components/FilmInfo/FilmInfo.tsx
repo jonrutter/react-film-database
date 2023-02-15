ec7631871df2cdf9d668d9a405df1f5e99c6a85e
Jon Rutter <contact@jonrutter.io>
2023-02-15 18:02:31 -0500

    Update FilmInfo
    - Various styling updates
    
    - Embed card image directly
    
    - Add accessible label for rating
    
    - Improve markup semantics

--- components/FilmInfo/FilmInfo.tsx
+++ components/FilmInfo/FilmInfo.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Image from 'next/image';
 import { formatMoney } from '@/utils/formatMoney';
 import { formatTime } from '@/utils/formatTime';
-import { Thumbnail } from '../Thumbnail/Thumbnail';
 import { Chip } from './Chip/Chip';
 import type { Crew } from '@/api/types';
 import NoImage from '@/public/no-avatar.svg';
@@ -32,45 +31,69 @@ export const FilmInfo: React.FC<Props> = ({
   budget,
   revenue,
 }) => (
-  <div className="relative w-full h-auto p-4">
-    <div className="relative h-full min-h-128 max-w-screen-xl mx-auto flex flex-col md:flex-row p-4 z-10 rounded-xl bg-zinc-800 bg-opacity-90">
-      <div className="relative w-full h-96 md:h-auto md:w-1/3">
-        <Thumbnail imgUrl={thumbUrl} label={title} />
-        <div className="absolute top-4 left-4 rounded-full bg-white w-10 h-10 flex justify-center items-center text-cyan-900 text-sm font-bold shadow-sm">
-          {rating.toFixed(1)}
+  <div className="relative w-full h-auto">
+    <div className="relative h-full max-w-screen-xl mx-auto px-6 py-4">
+      <div className="relative min-h-128 w-full h-full flex flex-col md:flex-row p-4 z-10 rounded-xl bg-slate-900/80">
+        <div className="relative w-full h-96 md:h-auto md:w-1/3">
+          <Image
+            src={thumbUrl || NoImage}
+            alt=""
+            placeholder="blur"
+            blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNMLe3eBAAExQIZHGZNfQAAAABJRU5ErkJggg=="
+            fill
+            className="object-cover rounded-lg pointer-events-none select-none"
+            sizes={
+              thumbUrl
+                ? '(max-width: 768px) 100vw, (max-width: 1200px) 33vw, 33vw'
+                : undefined
+            }
+          />
+          <div className="absolute top-4 left-4 rounded-full bg-white w-10 h-10 flex justify-center items-center text-cyan-900 text-sm font-bold shadow-sm">
+            <span className="sr-only">film rating: </span>
+            {rating.toFixed(1)}
+          </div>
         </div>
-      </div>
-      <div className="text-white px-0 py-4 md:py-0 text-center md:text-left md:px-8 w-full md:w-2/3">
-        <h2 className="text-2xl md:text-4xl font-bold pb-4">
-          {title} ({year})
-        </h2>
-        <h3 className="text-lg font-bold">Summary</h3>
-        <p className="mb-8 text-sm md:text-lg">{summary}</p>
-        <div>
+        <section className="text-white px-0 py-4 md:py-0 md:px-8 w-full md:w-2/3">
+          <h2 className="text-2xl md:text-4xl font-bold mb-4">
+            {title} ({year})
+          </h2>
+          <h3 className="text-lg md:text-xl font-bold mb-2">Summary</h3>
+          <p className="mb-8 text-base md:text-lg">{summary}</p>
           <div>
-            <h3 className="text-lg font-bold">
-              Director{directors.length > 1 ? 's' : ''}
-            </h3>
             <div>
-              {directors.map((director) => (
-                <p key={director.credit_id}>{director.name}</p>
-              ))}
+              <h3 className="text-lg md:text-xl font-bold mb-2">
+                Director{directors.length > 1 ? 's' : ''}
+              </h3>
+              <div>
+                {directors.map((director) => (
+                  <p
+                    key={director.credit_id}
+                    className="text-base md:text-lg mb-2"
+                  >
+                    {director.name}
+                  </p>
+                ))}
+              </div>
+            </div>
+            <div className="mt-8">
+              <h3 className="text-lg md:text-xl font-bold mb-2">Stats</h3>
+              <Chip
+                className="ml-0"
+                text={`Running time: ${formatTime(time)}`}
+              />
+              <Chip text={`Budget: ${formatMoney(budget)}`} />
+              <Chip text={`Revenue: ${formatMoney(revenue)}`} />
             </div>
           </div>
-          <div className="mt-8">
-            <h3 className="text-lg font-bold">Movie data</h3>
-            <Chip className="ml-0" text={`Running time: ${calcTime(time)}`} />
-            <Chip text={`Budget: ${convertMoney(budget)}`} />
-            <Chip text={`Revenue: ${convertMoney(revenue)}`} />
-          </div>
-        </div>
+        </section>
       </div>
     </div>
+
     <Image
       priority
       placeholder="blur"
       blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNMLe3eBAAExQIZHGZNfQAAAABJRU5ErkJggg=="
-      className="object-cover object-center"
+      className="object-cover object-center pointer-events-none select-none"
       fill
       src={backgroundImgUrl}
       alt=""
