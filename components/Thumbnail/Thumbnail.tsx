89b1a0c109929d71eaf6095100a71af772f84b69
Jon Rutter <contact@jonrutter.io>
2023-02-15 02:29:19 -0500

    Thumbnail updates
    - Use default image in component
    
    - Remove alt text
    
    - Add default size for remote images, and size for default image
    
    - Remove label prop0

--- components/Thumbnail/Thumbnail.tsx
+++ components/Thumbnail/Thumbnail.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import Image from 'next/image';
+import NoImage from '@/public/no-avatar.png';
 
 type Props = {
-  label: string;
-  imgUrl: string;
+  imgUrl?: string;
 };
 
-export const Thumbnail: React.FC<Props> = ({ label, imgUrl }) => (
+export const Thumbnail: React.FC<Props> = ({ imgUrl }) => (
   <Image
-    src={imgUrl}
-    alt={label}
+    src={imgUrl || NoImage}
+    alt=""
     placeholder="blur"
     blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNMLe3eBAAExQIZHGZNfQAAAABJRU5ErkJggg=="
     fill
     className="object-cover rounded-lg"
-    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 20vw"
+    sizes={
+      imgUrl
+        ? '(max-width: 768px) 100vw, (max-width: 1200px) 20vw, 20vw'
+        : '400px'
+    }
   />
 );
