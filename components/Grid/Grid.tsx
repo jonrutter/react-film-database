7f81fdd9083c0d6208608b39f9376a25d2b0ae30
Jon Rutter <contact@jonrutter.io>
2023-02-15 02:10:42 -0500

    Update heading styles
    Increase font size and margins

--- components/Grid/Grid.tsx
+++ components/Grid/Grid.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export const Grid: React.FC<Props> = ({ title, children, className }) => (
   <div className={className}>
-    <h2 className="text-xl font-bold pb-4">{title}</h2>
+    <h2 className="text-3xl font-bold mb-6">{title}</h2>
     <div className="grid grid-cols-auto-fill gap-8">{children}</div>
   </div>
 );
