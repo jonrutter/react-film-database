64be28fc3488620746a4ad86b5a9e43c8ed197f6
Jon Rutter <contact@jonrutter.io>
2023-02-15 18:13:48 -0500

    Update film detail page
    - Pass error
    
    - Handle API errors

--- pages/film/[id].tsx
+++ pages/film/[id].tsx
@@ -15,54 +15,75 @@ import { FilmInfo } from '@/components/FilmInfo/FilmInfo';
 import { Grid } from '@/components/Grid/Grid';
 import { Card } from '@/components/Card/Card';
 import Head from 'next/head';
+import Link from 'next/link';
 
 type Props = {
-  movie: Movie;
-  directors: Crew[];
-  cast: Cast[];
+  movie?: Movie;
+  directors?: Crew[];
+  cast?: Cast[];
+  error?: string;
 };
 
-const FilmPage: NextPage<Props> = ({ movie, directors, cast }) => (
+const FilmPage: NextPage<Props> = ({ movie, directors, cast, error }) => (
   <main>
     <Head>
-      <title>{movie.title} | React Film Database</title>
+      <title>{`${
+        movie?.title ? movie.title : 'Error'
+      } | React Film Database`}</title>
     </Head>
     <Header />
-    <Breadcrumb title={movie.title} />
-    <FilmInfo
-      thumbUrl={
-        movie.poster_path
-          ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
-          : '/no-image.jpg'
-      }
-      rating={movie.vote_average}
-      year={movie.release_date.split('-')[0]}
-      title={movie.title}
-      backgroundImgUrl={
-        movie.backdrop_path
-          ? IMAGE_BASE_URL + BACKDROP_SIZE + movie.backdrop_path
-          : '/no_image.jpg'
-      }
-      summary={movie.overview}
-      directors={directors}
-      time={movie.runtime}
-      budget={movie.budget}
-      revenue={movie.revenue}
-    />
-    <Grid className="p-4 max-w-screen-xl mx-auto" title="Actors">
-      {cast.map((actor) => (
-        <Card
-          key={actor.credit_id}
-          imgUrl={
-            actor.profile_path
-              ? IMAGE_BASE_URL + POSTER_SIZE + actor.profile_path
+    {movie && directors && cast ? (
+      <>
+        <Breadcrumb title={movie.title} />
+        <FilmInfo
+          thumbUrl={
+            movie.poster_path
+              ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
               : '/no_image.jpg'
           }
-          title={actor.name}
-          subtitle={actor.character}
+          rating={movie.vote_average}
+          year={movie.release_date.split('-')[0]}
+          title={movie.title}
+          backgroundImgUrl={
+            movie.backdrop_path
+              ? IMAGE_BASE_URL + BACKDROP_SIZE + movie.backdrop_path
+              : '/no_image.jpg'
+          }
+          summary={movie.overview}
+          directors={directors}
+          time={movie.runtime}
+          budget={movie.budget}
+          revenue={movie.revenue}
         />
-      ))}
-    </Grid>
+        <Grid className="px-6 py-6 max-w-screen-xl mx-auto" title="Cast">
+          {cast.map((actor) => (
+            <Card
+              key={actor.credit_id}
+              imgUrl={
+                actor.profile_path
+                  ? IMAGE_BASE_URL + POSTER_SIZE + actor.profile_path
+                  : ''
+              }
+              title={actor.name}
+              subtitle={actor.character}
+            />
+          ))}
+        </Grid>
+      </>
+    ) : (
+      <div className="px-6">
+        <p className="mt-12 mb-4">
+          Oops! Sorry, there was a problem getting the movie details.
+        </p>
+        <p>
+          Please try going back{' '}
+          <Link href="/" className="underline">
+            home
+          </Link>
+          .
+        </p>
+      </div>
+    )}
   </main>
 );
 
@@ -74,19 +95,28 @@ export const getStaticProps: GetStaticProps = async (context) => {
   const movieEndPoint: string = movieUrl(id);
   const creditsEndpoint: string = creditsUrl(id);
 
-  const movie = await baseFetch<Movie>(movieEndPoint);
-  const credits = await baseFetch<Credits>(creditsEndpoint);
-
-  // get the directors only
-  const directors = credits.crew.filter((member) => member.job === 'Director');
-  return {
-    props: {
-      movie,
-      directors,
-      cast: credits.cast,
-    },
-    revalidate: 60 * 60 * 24, // only cache page for 24 hours
-  };
+  try {
+    const movie = await baseFetch<Movie>(movieEndPoint);
+    const credits = await baseFetch<Credits>(creditsEndpoint);
+    // get the directors only
+    const directors = credits.crew.filter(
+      (member) => member.job === 'Director'
+    );
+    return {
+      props: {
+        movie,
+        directors,
+        cast: credits.cast,
+      },
+      revalidate: 60 * 60 * 24, // only cache page for 24 hours
+    };
+  } catch (err) {
+    return {
+      props: {
+        error: 'Error fetching from API',
+      },
+    };
+  }
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
