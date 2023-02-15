7dce921ef7ef5f15b98cbfd211aebe43102323c9
Jon Rutter <contact@jonrutter.io>
2023-02-15 16:08:26 -0500

    Update Header
    - Use custom logos
    
    - Update styling
    
    - Use h1 tag

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { SearchInput } from './SearchInput/SearchInput';
-import RMDBLogo from '@/public/rmdb-logo.svg';
-import RMDBLogoSmall from '@/public/rmdb-logo-small.svg';
+import LogoFull from '@/public/logo-full.svg';
+import LogoSmall from '@/public/logo-small.svg';
 
 type Props = {
   setQuery?: React.Dispatch<React.SetStateAction<string>>;
@@ -11,23 +11,29 @@ type Props = {
 
 /** Renders the site header. */
 export const Header: React.FC<Props> = ({ setQuery }) => (
-  <div className="sticky flex top-0 z-40 w-full h-24 bg-zinc-900">
-    <div className="flex flex-wrap justify-between items-center w-full h-full max-w-screen-xl mx-auto px-4">
-      <Link href="/">
-        <div className="flex items-center cursor-pointer">
-          <div className="hidden md:block">
-            <Image src={RMDBLogo} alt="React Movie Database" />
+  <div className="sticky flex top-0 z-40 w-full bg-gradient-to-br from-slate-900 to-slate-800 shadow-lg">
+    <div className="flex flex-wrap justify-between items-center w-full h-full max-w-screen-xl mx-auto px-6 py-2 md:py-4">
+      <h1>
+        <Link href="/">
+          <div className="flex items-center cursor-pointer py-2">
+            <div className="hidden sm:block">
+              <Image
+                src={LogoFull}
+                alt="React Film Database"
+                className="h-6 w-full"
+              />
+            </div>
+            <div className="sm:hidden">
+              <Image
+                src={LogoSmall}
+                alt="React Film Database"
+                className="h-6 w-full"
+              />
+            </div>
           </div>
-          <div className="md:hidden">
-            <Image src={RMDBLogoSmall} alt="React Movie Database" />
-          </div>
-        </div>
-      </Link>
-      {setQuery ? (
-        <div className="relative flex items-center">
-          <SearchInput setQuery={setQuery} />
-        </div>
-      ) : null}
+        </Link>
+      </h1>
+      {setQuery ? <SearchInput setQuery={setQuery} /> : null}
     </div>
   </div>
 );
