60aba86e230a8684e1538dec2cfcce84d9a42c1d
Jon Rutter <contact@jonrutter.io>
2023-05-10 15:23:39 -0400

    Improve Hero content
    - Clarify title
    
    - Incrase spacing
    
    - Improve link focus state
    
    - Top-align backdrop image

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -14,16 +14,18 @@ export const Hero: React.FC<Props> = ({ imgUrl, title, text, id }) => (
     <div className="relative flex flex-col-reverse h-full max-w-screen-xl mx-auto z-10 pb-12 text-left">
       <section className="text-white max-w-2xl px-6">
         <h2 className="font-bold mb-3 md:mb-6 flex flex-col">
-          <span className="text-xl md:text-3xl mb-1 md:mb-2">Top film:</span>
+          <span className="text-xl md:text-3xl mb-1 md:mb-2 text-slate-200">
+            Today's top film:
+          </span>
           <span className="text-3xl md:text-5xl ">{title}</span>
         </h2>
         <p className="text-lg mb-4 md:hidden">
           {text.length > 200 ? text.slice(0, 196) + '...' : text}
         </p>
-        <p className="text-lg mb-4 hidden md:block">{text}</p>
+        <p className="text-lg mb-6 hidden md:block">{text}</p>
         <Link
           href={`/film/${id}`}
-          className="bg-cyan-900 rounded-full py-2 px-6 text-lg text-white inline-block hover:opacity-80 transition-opacity duration-300 focus:outline-none focus:ring-2 focus:ring-white font-semibold"
+          className="bg-cyan-700 rounded-full py-2 px-6 text-lg text-white inline-block hover:bg-cyan-600 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-white font-semibold focus:ring-offset-2 focus:ring-offset-slate-900"
         >
           Full details
         </Link>
@@ -32,7 +34,7 @@ export const Hero: React.FC<Props> = ({ imgUrl, title, text, id }) => (
     <Image
       priority
       fill
-      className="object-cover object-center -z-10"
+      className="object-cover object-top -z-10 animate-fade-in"
       src={imgUrl}
       alt="hero"
       quality={100}
